e564993b7f3d1db0bf33d9fabc5cfd77483b185e
Rick Paxton <rick.paxton@encore.tech>
2021-02-25 16:53:32 -0500

    Update spec file

--- spec/classes/install/sssd_spec.rb
+++ spec/classes/install/sssd_spec.rb
@@ -11,21 +11,10 @@ describe 'ipa::install::sssd' do
         <<-'EOS'
 #!/bin/bash
 
-check_command_exists() {
-  (which "$1") > /dev/null 2>&1
-  command_exists=$?
-  return $command_exists
-}
-
-check_command_exists "systemctl"
-is_systemd=$?
-check_command_exists "service"
-is_service=$?
-
 service_mgt() {
   action="$1"
   name="$2"
-  if [ $is_systemd -eq 0 ]; then
+  if [[ -L '/sbin/init' ]]; then
     systemctl "$action" "$name"
   else
     service "$name" "$action"
@@ -33,10 +22,12 @@ service_mgt() {
   return $?
 }
 
+echo "Stopping sssd service and clearing cache."
 service_mgt "stop" "sssd"
 rm -rf /var/lib/sss/db/* /var/lib/sss/mc/*
-service_mgt "start" "sssd"
 
+echo "Restarting sssd services."
+service_mgt "start" "sssd"
 service_mgt "restart" "autofs"
 
 service_mgt "status" "sssd"
